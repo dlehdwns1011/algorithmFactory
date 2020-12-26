4bf59f9e0d17fcb536ff6aa0800fce26cf7820d0
dlehdwns1011 <46744149+dlehdwns1011@users.noreply.github.com>
2020-12-26 22:43:47 +0900

    Add files via upload

---
+++ 3143. 가장 빠른 문자열 타이핑.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int testnum = 0;
+	cin >> testnum;
+
+	
+	for (int test = 0; test < testnum; ++test)
+	{
+		string a = "";
+		string b = "";
+		cin >> a;
+		cin >> b;
+		int result = 0;
+
+		while (1)
+		{
+			int findindex = a.find(b);
+			if (findindex == -1)
+				break;
+
+			++result;
+			a.erase(a.begin() + findindex, a.begin() + findindex + b.size());
+		}
+		cout << "#" << test + 1 << " " << result + a.size() << "\n";
+	}
+
+	return 0;
+}
