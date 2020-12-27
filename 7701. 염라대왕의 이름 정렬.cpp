a04d28e4e176505dd6e6d0cb4fa7112088e10f0e
dlehdwns1011 <46744149+dlehdwns1011@users.noreply.github.com>
2020-12-27 23:30:12 +0900

    Add files via upload

---
+++ 7701. 염라대왕의 이름 정렬.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int testnum = 0;
+	cin >> testnum;
+	for (int itest = 0; itest < testnum; ++itest)
+	{
+		int testcase = 0; 
+		cin >> testcase;
+		vector< pair<int,string> > text;
+		for (int icase = 0; icase < testcase; ++icase)
+		{
+			string input;
+			cin >> input;
+			text.emplace_back(input.size(), input);
+		}
+		sort(text.begin(), text.end());
+		cout << "#" << itest + 1 << "\n";
+		text.erase(unique(text.begin(), text.end()), text.end());
+		int textsize = text.size();
+		for (int i = 0; i < textsize; ++i)
+		{
+			cout << text[i].second<<"\n";
+		}
+	}
+
+	return 0;
+}
