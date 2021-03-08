59add843fc77ecaaa69c8d602b4c37000ef2d68e
dlehdwns1011 <46744149+dlehdwns1011@users.noreply.github.com>
2021-03-09 00:14:52 +0900

    SWEA

--- 석찬이의 받아쓰기.cpp
+++ 석찬이의 받아쓰기.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int test = 0; cin >> test;
+
+	for (int testnum = 1; testnum <= test; ++testnum)
+	{
+		int n; cin >> n;
+		char string1[100001], string2[100001];
+		cin >> string1 >> string2;
+
+		int result = 0;
+		for (int i = 0; i < n; ++i)
+			if (string1[i] == string2[i])
+				++result;
+
+		cout << "#" << testnum << " " << result << "\n";
+	}
+
+	return 0;
+}
